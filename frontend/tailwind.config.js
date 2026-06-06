2960df29cab465dfd7823450f2397117fd62298f
César Torrontegui <cesarbustos871@gmail.com>
2026-06-05 22:24:56 -0600

    Día 8: Desarrollo de interfaz principal en React. Implementación de carrusel dinámico UI/UX con Tailwind CSS y configuración de react-router-dom

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Agrega esto dentro de tu tailwind.config.js -> theme -> extend
+      keyframes: {
+        'fade-in-right': {
+          '0%': { opacity: '0', transform: 'translateX(20px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      colors: {
+        cobrizo: '#2C1B14', 
+        acento: '#E87A5D'
+      }
+    },
+  },
+  plugins: [],
+}
